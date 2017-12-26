756e5f7c1f1c5923ad6f0b151151221fc9c59bc7
inzaghihao <wangyipippo@163.com>
2017-12-26 10:54:36 +0800

    登录

--- app.js
+++ app.js
@@ -1,64 +1,68 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    // var logs = wx.getStorageSync('logs') || []
-    // logs.unshift(Date.now())
-    // wx.setStorageSync('logs', logs)
 
     // 登录
-    wx.login({
-
-      success: res => {
-        wx.getUserInfo({
-            success: function (userResult) {
-                // 发送 res.code 到后台换取 openId, sessionKey, unionId
-                if (res.code) {
-                    //发起网络请求
-                    var header = {};
-
-                    header['X-WX-Code'] = res.code;
-                    header['X-WX-Encrypted-Data'] = userResult.encryptedData;
-                    header['X-WX-IV'] = userResult.iv;
-                    wx.request({
-                        url: 'https://www.yingshangyan.com/wxapi/login',
-                        header: header,
-                        data: {
-                            code: res.code
-                        }
-                    })
-                } else {
-                    console.log('获取用户登录态失败！' + res.errMsg)
-                }
-            },
-
-            fail: function (userError) {
-                console.log('ERR_WX_GET_USER_INFO', '获取微信用户信息失败，请检查网络状态');
-            },
-        });
-
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+    var that = this;
+    wx.checkSession({
+      success: function () {
+        //session 未过期，并且在本生命周期一直有效
+      },
+      fail: function () {
+        //登录态过期
+        //重新登录
+        wx.login({
+          success: res => {
+            wx.request({
+              url: 'https://www.yingshangyan.com/wxapi/login',
+              header: {
+                'content-type': 'application/json'
+              },
+              data: {
+                code: res.code
+              },
+              success(res) {
+                // wx.setStorageSync('__userid__', userid)
+                console.log(res.data)
               }
-            }
-          })
-        }
+            })
+            // wx.getUserInfo({
+            //     success: function (userResult) {
+            //         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+            //         if (res.code) {
+            //             //发起网络请求
+            //             var header = {};
+
+            //             header['X-WX-Code'] = res.code;
+            //             header['X-WX-Encrypted-Data'] = userResult.encryptedData;
+            //             header['X-WX-IV'] = userResult.iv;
+            //             wx.request({
+            //                 url: 'https://www.yingshangyan.com/wxapi/login',
+            //                 header: header,
+            //                 data: {
+            //                     code: res.code
+            //                 },
+            //                 success(res){
+            //                   var userid = res.data.data.userinfo
+            //                   wx.setStorageSync('__userid__', userid)
+            //                   //console.log(res.data.data.userinfo)
+            //                 }
+            //             })
+            //         } else {
+            //             console.log('获取用户登录态失败！' + res.errMsg)
+            //         }
+            //     },
+
+            //     fail: function (userError) {
+            //         console.log('ERR_WX_GET_USER_INFO', '获取微信用户信息失败，请检查网络状态');
+            //     },
+            // });
+
+          }
+        })
       }
     })
+   
   },
   globalData: {
     userInfo: null
--- pages/select/select.js
+++ pages/select/select.js
@@ -1,4 +1,5 @@
 const util = require('../../utils/util.js');
+const app = getApp();
 Page({
   data: {
     hotCity: [],
